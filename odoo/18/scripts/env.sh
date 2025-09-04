c2baeb15659e72c622666b0a3486cf309fbb1645
Amin Cheloh <amincheloh@gmail.com>
2025-09-04 02:46:49 +0000

    feat(odoo): limit time config

--- odoo/18/scripts/env.sh
+++ odoo/18/scripts/env.sh
@@ -9,6 +9,8 @@ export ODOO_CONF_TEMPLATE_FILE="/opt/scripts/odoo.conf.j2"
 export ODOO_CONF_FILE="/etc/odoo/odoo.conf"
 export ODOO_SKIP_BOOTSTRAP="${ODOO_SKIP_BOOTSTRAP:-false}"
 export ODOO_LOAD_DEMO_DATA="${ODOO_LOAD_DEMO_DATA:-false}"
+export ODOO_LIMIT_TIME_CPU="${ODOO_LIMIT_TIME_CPU:-600}"
+export ODOO_LIMIT_TIME_REAL="${ODOO_LIMIT_TIME_REAL:-1200}"
 #
 export ODOO_EMAIL="${ODOO_EMAIL:-admin@example.com}"
 export ODOO_PASSWORD="${ODOO_PASSWORD:-odooadmin}"
